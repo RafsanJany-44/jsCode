2959404b26e4130d25954b29eaec29edacc3bc86
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-05-04 15:10:50 +0600

    llll

--- jquery/script.js
+++ jquery/script.js
@@ -15,3 +15,19 @@ doc.innerHTML="change valu for head2";
 
 
 document.getElementById("pera1").innerHTML="pera 1 changed";
+
+
+// to change with a tag name and index
+
+document.getElementsByTagName("h2")[3].innerHTML="hello world";
+
+
+
+// above oparation can be done by query selector
+document.querySelector("#pera2").innerHTML="this is a wuery wlector";
+document.querySelector(".pera3").innerHTML="this is a wuery wlector";
+
+
+// parrent child
+document.querySelector(".my-div a").innerHTML="-----------";
+

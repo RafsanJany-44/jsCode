0b6987cc097cc188d20c904e9fce0865009d9bec
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2021-05-04 14:43:50 +0600

    js object

---
+++ jquery/script.js
@@ -0,0 +1,17 @@
+
+// to change any text 
+document.querySelector("h2").innerHTML="HI";
+
+
+//to change any element with id
+
+document.getElementById("head1").innerHTML=" chanege value of head1";
+
+
+// to change with a variable
+
+var doc=document.getElementById("head2");
+doc.innerHTML="change valu for head2";
+
+
+document.getElementById("pera1").innerHTML="pera 1 changed";
